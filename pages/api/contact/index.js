8c1a57d719f7aa6ccfc7d95bc48a7d5900212b89
Alex Miranda <alexxmirandaa@gmail.com>
2024-04-17 21:24:05 -0300

    wip

---
+++ pages/api/contact/index.js
@@ -0,0 +1,33 @@
+import { Resend } from "resend";
+import Cors from "cors";
+
+const resend = new Resend("re_cvnsCxLN_AWqktGWXWK88juAy9yLaYHSN");
+
+const cors = Cors({
+  origin: "https://www.brielab.com.br",
+  methods: ["POST", "GET", "HEAD"],
+});
+
+export default async function handler(req, res) {
+  const {
+    body: { nome, email, assunto, mensagem },
+  } = req;
+
+  const { data, error } = await resend.emails.send({
+    from: "Site Bri <onboarding@resend.dev>",
+    to: ["alexxmirandaa@gmail.com"],
+    subject: "Hello world",
+    text: `
+      nome: ${nome} \n
+      email: ${email} \n
+      assunto: ${assunto} \n
+      mensagem: ${mensagem} \n
+    `,
+  });
+
+  if (error) {
+    return error;
+  }
+
+  res.status(200).json(data);
+}
