05b31adbc72a3743cfccc5961670b31e3bd9d284
Alex Miranda <alexxmirandaa@gmail.com>
2024-05-16 23:11:28 -0300

    add script do google analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,17 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+          rel="stylesheet"
+        />
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-C3QF2KCW1G');
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
 export default MyApp
