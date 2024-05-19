e839701ceab09dff3629d4f57361e20d4ef8c5e4
Alex Miranda <alexxmirandaa@gmail.com>
2024-05-18 21:19:49 -0300

    add script do GA com o componente Script do next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head'
+import Script from 'next/script';
 import '../styles/globals.css'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-C3QF2KCW1G');
+  }, [])
   return (
     <>
       <Head>
@@ -11,10 +21,11 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
           rel="stylesheet"
         />
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {window.dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-C3QF2KCW1G');
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-C3QF2KCW1G"
+      />
       <Component {...pageProps} />
     </>
   );
